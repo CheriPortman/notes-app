86cf704e703becec1556a277a28ed2b783a4fafc
Cheri Portman <cheriportman@hotmail.com>
2019-06-05 18:05:10 -0700

    fixed error in api call

--- src/services/notesApi.js
+++ src/services/notesApi.js
@@ -1,14 +1,12 @@
-import React from 'react';
-
 export const createNote = note => {
   return fetch('some url', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(note)
   })
-  .then(res => ([res.ok, res.json()]))
-  .then(([ok, json]) => {
-    if(!ok) throw 'Unable to fetch characters';
-
-    console.log(json);
-  };
+    .then(res => ([res.ok, res.json()]))
+    .then(([ok, json]) => {
+      if(!ok) throw 'cannot find your notes';
+      console.log(json);
+    });
+};
